662eac958633816bf45a294943c81c143868bb83
Junaid Choudhary <junaid@fantoz.tech>
2023-03-02 09:36:09 +0530

    remove build

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ export default function Router() {
       path: '/dashboard',
       element: <DashboardLayout />,
       children: [
-        { element: <Navigate to="/dashboard/app" /> },
+        { element: <Navigate to="/dashboard/app" />, index: true },
         { path: 'app', element: <DashboardAppPage /> },
         { path: 'user', element: <UserPage /> },
         { path: 'products', element: <ProductsPage /> },
